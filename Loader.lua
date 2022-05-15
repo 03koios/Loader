d6ff728e7ce1c7624b33cf200ca2f215d8ad55d2
03koios <102623036+03koios@users.noreply.github.com>
2022-05-14 13:56:07 -1200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDiscord03s/main/README.md"))()
 
 --- Loader/GamePlace.id
-loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/discord.gg-x4gGhjVxXz/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/KeySystem/main/.lua"))()
 
 --- discord.gg/x4gGhjVxXz
