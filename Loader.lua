fcbbbeb990fdd01a3f94b7e9ef63cdd42330a7d6
03koios <102623036+03koios@users.noreply.github.com>
2022-08-20 20:29:08 -1200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/KeySystem/main/.lua"))()
+
+
+--- You Can delet this if you want
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Syn/main/README.md"))()
